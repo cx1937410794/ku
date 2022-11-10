c6c16be7e2aa29dd4a00cabb76f884d05e85385c
cx1937410794 <104968346+cx1937410794@users.noreply.github.com>
2022-11-10 16:33:40 +0800

    更新 积分二.js

--- 0mk/控制/积分二.js
+++ 0mk/控制/积分二.js
@@ -47,7 +47,7 @@ function 检查使用权限() { //检查id函数
         idlujing.put("idlujing", jzzxid[1]); //存ID
         threads.start(function () {
             function 到期时间() {
-                let res = http.get("https://zuoxiaozi.top/0mk/UI/右侧账号.json", { headers: { 'Accept-Language': 'zh-cn,zh;q=0.5', 'User-Agent': 'Mozilla/5.0(Macintosh;IntelMacOSX10_7_0)AppleWebKit/535.11(KHTML,likeGecko)Chrome/17.0.963.56Safari/535.11' } });
+                let res = http.get("https://raw.gh.fakev.cn/cx1937410794/ku/main/0mk/UI/右侧账号.json", { headers: { 'Accept-Language': 'zh-cn,zh;q=0.5', 'User-Agent': 'Mozilla/5.0(Macintosh;IntelMacOSX10_7_0)AppleWebKit/535.11(KHTML,likeGecko)Chrome/17.0.963.56Safari/535.11' } });
                 if (res.statusCode != 200) { alert("❌获取失败: " + res.statusCode); return; }
                 let json = res.body.json();
                 let thisTime = json[jzzxid[1]];
@@ -76,7 +76,7 @@ function 检查使用权限() { //检查id函数
                 console.hide() //关闭日志窗口
                 exit();
             } else if (0 > 结果i) {
-                let res = http.get("https://zuoxiaozi.top/0mk/UI/右侧账号.json", { headers: { 'Accept-Language': 'zh-cn,zh;q=0.5', 'User-Agent': 'Mozilla/5.0(Macintosh;IntelMacOSX10_7_0)AppleWebKit/535.11(KHTML,likeGecko)Chrome/17.0.963.56Safari/535.11' } });
+                let res = http.get("https://raw.gh.fakev.cn/cx1937410794/ku/main/0mk/UI/右侧账号.json", { headers: { 'Accept-Language': 'zh-cn,zh;q=0.5', 'User-Agent': 'Mozilla/5.0(Macintosh;IntelMacOSX10_7_0)AppleWebKit/535.11(KHTML,likeGecko)Chrome/17.0.963.56Safari/535.11' } });
                 if (res.statusCode != 200) { console.error("❌获取失败: " + res.statusCode); return; };
                 let json = res.body.json();
                 let thisTime = json[jzzxid[1]];
