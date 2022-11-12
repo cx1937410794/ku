b14176a40fd8f587b4b5507602222d8d53e952a4
cx1937410794 <104968346+cx1937410794@users.noreply.github.com>
2022-11-12 18:22:10 +0800

    1

--- 说明.js
+++ 说明.js
@@ -2,13 +2,17 @@
 
 zuoxiaozi.top
 
-raw.gh.fakev.cn/cx1937410794/ku/main
+raw.gh.fakev.cn / cx1937410794 / ku / main
 
 
 
 //信息替换
-title="{{unescape('%u5F3A%u56FD%u52A9%u624B')}}"
+title = "{{unescape('%u5F3A%u56FD%u52A9%u624B')}}"
 // https://tool.chinaz.com/tools/urlencode.aspx
 
 
-{{unescape('%E6%9F%A5%E8%AF%A2%E5%86%9B%E8%81%8C%E5%9C%A8%E7%BA%BFID')}}
+{ { unescape('%E6%9F%A5%E8%AF%A2%E5%86%9B%E8%81%8C%E5%9C%A8%E7%BA%BFID') } }
+
+
+
+title = "{{unescape('%E6%9F%A5%E8%AF%A2%E5%86%9B%E8%81%8C%E5%9C%A8%E7%BA%BFID')}}"
