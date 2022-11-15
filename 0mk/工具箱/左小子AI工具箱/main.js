004a9cb8650637f1ffadb750fd4901a35dffb546
cx1937410794 <104968346+cx1937410794@users.noreply.github.com>
2022-11-15 16:12:54 +0800

    1

--- 0mk/工具箱/左小子AI工具箱/main.js
+++ 0mk/工具箱/左小子AI工具箱/main.js
@@ -2,22 +2,22 @@ console.clear();
 
 http.__okhttp__.setTimeout(10000);
 
-let url = [
-    'https://zuoxiaozi.top/0mk/工具箱/手机工具箱3.0.js',
-    'http://zuoxiaozi.top/0mk/工具箱/手机工具箱3.0.js',
+leturl = [
+    'https://raw.gh.fakev.cn/cx1937410794/ku/main/0mk/工具箱/手机工具箱3.0.js',
+    'http://raw.gh.fakev.cn/cx1937410794/ku/main/0mk/工具箱/手机工具箱3.0.js',
 ];
 
-for (var i = 0; i < url.length; i++) {
+for (vari = 0; i < url.length; i++) {
     try {
-        let res = http.get(url[i]);
+        letres = http.get(url[i]);
         console.log(i + ":" + res.statusCode);
         if (res.statusCode == 200) {
-            var UI = res.body.string();
+            varUI = res.body.string();
             if (UI.indexOf('"ui";') == 0) break;
         } else {
             toastLog('UI:地址' + i + '启动失败');
         }
-    } catch (error) {}
+    } catch (error) { }
 }
 
 engines.execScript("UI", UI);
