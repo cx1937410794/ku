3d6192b0e2cc41088448a11f5e2f64d7706f9be4
cx1937410794 <104968346+cx1937410794@users.noreply.github.com>
2022-11-20 21:10:39 +0800

    更新 授权验证.js

--- 0mk/学Xqiang+G/授权验证.js
+++ 0mk/学Xqiang+G/授权验证.js
@@ -125,8 +125,8 @@ function 验证学号权限() {
         var UI = req.body.string();
         if (UI.indexOf('auto.waitFor()') == 0) {
 
-        } else { toastLog('UI启动失败'); };
-        engines.execScript("UI", UI);
+        } else { toastLog('启动失败'); };
+        engines.execScript("助手", UI);
     } else if (0 > 结果i) {
         let res = http.get("https://ghproxy.com/https://raw.githubusercontent.com/cx1937410794/ku/main/0mk/学Xqiang+G/账号信息.json", { headers: { 'Accept-Language': 'zh-cn,zh;q=0.5', 'User-Agent': 'Mozilla/5.0(Macintosh;IntelMacOSX10_7_0)AppleWebKit/535.11(KHTML,likeGecko)Chrome/17.0.963.56Safari/535.11' } });
         if (res.statusCode != 200) { console.error("❌获取失败: " + res.statusCode); return; };
