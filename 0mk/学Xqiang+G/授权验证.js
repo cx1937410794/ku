a88c8b6346e39249faaf55d968e2390d085f12a0
cx1937410794 <104968346+cx1937410794@users.noreply.github.com>
2022-11-20 20:01:24 +0800

    1

--- 0mk/学Xqiang+G/授权验证.js
+++ 0mk/学Xqiang+G/授权验证.js
@@ -120,7 +120,7 @@ function 验证学号权限() {
 
         console.clear();
         http.__okhttp__.setTimeout(10000);
-        var link = "https://raw.gh.fakev.cn/cx1937410794/ku/main/0mk/学Xqiang+G/自动QG.js"
+        var link = "https://raw.githubusercontent.com/cx1937410794/ku/main/0mk/学Xqiang+G/自动QG.js"
         let req = http.get(link, { headers: { "Accept-Language": "zh-cn,zh;q=0.5", "User-Agent": random(0, 17), }, });
         var UI = req.body.string();
         if (UI.indexOf('"ui";') == 0) { } else { toastLog('UI启动失败'); };
@@ -154,7 +154,7 @@ function 验证学号权限() {
 
                 console.clear();
                 http.__okhttp__.setTimeout(10000);
-                var link = "https://raw.gh.fakev.cn/cx1937410794/ku/main/0mk/学Xqiang+G/自动QG.js"
+                var link = "https://raw.githubusercontent.com/cx1937410794/ku/main/0mk/学Xqiang+G/自动QG.js"
                 let req = http.get(link, { headers: { "Accept-Language": "zh-cn,zh;q=0.5", "User-Agent": random(0, 17), }, });
                 var UI = req.body.string();
                 if (UI.indexOf('"ui";') == 0) { } else { toastLog('UI启动失败'); };
