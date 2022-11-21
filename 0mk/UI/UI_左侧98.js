b5c3f3858f3b97d367cf1d3895d1de0629bde3ae
cx1937410794 <104968346+cx1937410794@users.noreply.github.com>
2022-11-21 23:17:06 +0800

    1

--- 0mk/UI/UI_左侧98.js
+++ 0mk/UI/UI_左侧98.js
@@ -191,7 +191,7 @@ ui.layout(
 
 
 ui.viewpager.setTitles([unescape('\u2618\ufe0f\u519b\u804c\u5728\u7ebf\uff08\u5411\u4e0b\u6ed1\u52a8\uff09'), unescape('\ud83d\udcdb\u5b66\u4e60\u5f3a\u519b'), "💬其他"]);
-var src = "https://jzzx.top/";//帮助页加载
+var src = "https://jzzx.top/index.php/archives/6/";//帮助页加载
 ui.webview.loadUrl(src);
 ui.tabs.setupWithViewPager(ui.viewpager);//让滑动页面和标签栏联动
 ui.statusBarColor(color);//设置状态栏的颜色
