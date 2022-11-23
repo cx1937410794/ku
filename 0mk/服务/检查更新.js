428df55c62a678edc8ba19c273ec6caf41c79346
cx1937410794 <104968346+cx1937410794@users.noreply.github.com>
2022-11-23 13:29:05 +0800

    更新 检查更新.js

--- 0mk/服务/检查更新.js
+++ 0mk/服务/检查更新.js
@@ -10,15 +10,20 @@ packageInfo = pm.getPackageInfo(packageName, 0)
 var 自身版本名称 = packageInfo.versionName
 // alert(自身版本名称);
 
+// https://zuoxiaozi.lanzouv.com/b03n9vqle
 threads.start(function () {
-    更新功能("https://zuoxiaozi.lanzouy.com/iOZfM0gky5pg", "1.重新打造笔记页面\n2.新增微/Q/支刷步数功能\n3.优化底层代码\n4.笔记输入“进入助手左”保存，下次打开直接进入助手(根据个人需求使用)", "101")
+    更新功能(
+        "iRBM90gnw1be",
+        "1.重新打造笔记页面\n2.新增微/Q/支刷步数功能\n3.优化底层代码\n4.笔记输入“进入助手左”保存，下次打开直接进入助手(根据个人需求使用)",
+        "101"
+    );
 });
 
 
 function 更新功能(新版本连接, 更新公告, 更新版本号) {
     if (自身版本名称 == 更新版本号) { toastLog("最新版本！"); } else {
         threads.start(function () {
-            let result = http.get("https://api.ooomn.com/api/lanzou?type=down&url=" + 新版本连接, { headers: { 'Accept-Language': 'zh-cn,zh;q=0.5', 'User-Agent': 'Mozilla/5.0(Macintosh;IntelMacOSX10_7_0)AppleWebKit/535.11(KHTML,likeGecko)Chrome/17.0.963.56Safari/535.11' } });
+            let result = http.get("https://api.ooomn.com/api/lanzou?type=down&url=https://zuoxiaozi.lanzouy.com/" + 新版本连接, { headers: { 'Accept-Language': 'zh-cn,zh;q=0.5', 'User-Agent': 'Mozilla/5.0(Macintosh;IntelMacOSX10_7_0)AppleWebKit/535.11(KHTML,likeGecko)Chrome/17.0.963.56Safari/535.11' } });
             var 下载直链; if (result.statusCode == 200) { 下载直链 = result.url; 检查更新(); } else { alert("❌请求错误，请联系管理员"); };
 
 
