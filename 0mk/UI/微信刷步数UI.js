a61d99af62d837ef1d06e1420281431368286e38
cx1937410794 <104968346+cx1937410794@users.noreply.github.com>
2022-11-29 00:25:55 +0800

    更新 微信刷步数UI.js

--- 0mk/UI/微信刷步数UI.js
+++ 0mk/UI/微信刷步数UI.js
@@ -4,10 +4,8 @@ function 每日一言() {
     threads.start(function () {
         let result = http.get("https://v.api.aa1.cn/api/api-wenan-anwei/index.php?type=json", { headers: { 'Accept-Language': 'zh-cn,zh;q=0.5', 'User-Agent': 'Mozilla/5.0(Macintosh;IntelMacOSX10_7_0)AppleWebKit/535.11(KHTML,likeGecko)Chrome/17.0.963.56Safari/535.11' } });
         if (result.statusCode == 200) {
-            // ui.run(() => { ui.yiyan.setText(result.body.json().anwei); });
-
-
-            alert("一句话才是最好的鼓励\n\n" + result.body.json().anwei)
+            // ui.run(() => { ui.yiyan.setText(result.body.json().anwei); }); 
+            alert(result.body.json().anwei)
         };
     });
 };
