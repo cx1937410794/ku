b7e8b049ccc40e64a56d5182e02da95d9ea6bd5a
cx1937410794 <104968346+cx1937410794@users.noreply.github.com>
2022-11-29 00:24:17 +0800

    更新 微信刷步数UI.js

--- 0mk/UI/微信刷步数UI.js
+++ 0mk/UI/微信刷步数UI.js
@@ -1,4 +1,5 @@
 "ui";
+
 function 每日一言() {
     threads.start(function () {
         let result = http.get("https://v.api.aa1.cn/api/api-wenan-anwei/index.php?type=json", { headers: { 'Accept-Language': 'zh-cn,zh;q=0.5', 'User-Agent': 'Mozilla/5.0(Macintosh;IntelMacOSX10_7_0)AppleWebKit/535.11(KHTML,likeGecko)Chrome/17.0.963.56Safari/535.11' } });
