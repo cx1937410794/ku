5fe8df4111bf104f64f477b4610d7ff9903ce95c
cx1937410794 <104968346+cx1937410794@users.noreply.github.com>
2022-12-19 12:09:50 +0800

    更新 2视频播放器.js

--- 000直播神器/2视频播放器.js
+++ 000直播神器/2视频播放器.js
@@ -1,5 +1,7 @@
 var idlujing = storages.create("shuju"); //被存的数据路径
 toast("切记:请勿相信任何广告");
+toast("切记:请勿相信任何广告");
+toast("切记:请勿相信任何广告");
 importClass(android.webkit.MimeTypeMap);
 importClass(android.net.Uri)
 let url = idlujing.get("主播链接"); //示例，实际填你的网络视频链接
