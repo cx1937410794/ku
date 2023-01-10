15d4b76c1b8abc7ac1354e903d0371e3ac64df8f
cx1937410794 <104968346+cx1937410794@users.noreply.github.com>
2023-01-10 09:25:40 +0800

    更新 UI_左侧98.js

--- 0mk/UI/UI_左侧98.js
+++ 0mk/UI/UI_左侧98.js
@@ -1,4 +1,7 @@
 "ui";
+alert("本版本停止维护\n请下载最新版本\n官网:jzzx.top/0mk");
+
+exit();
 var idlujing = storages.create("shuju"); //被存的数据路径
 var moocStorage = storages.create("shuju"); //被存的数据路径每日任务
 importClass(java.net.HttpURLConnection);
